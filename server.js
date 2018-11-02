69d8adf3788e5092ab565b36b4d6a65d40d389cf
Cyntia <cyntia.aparecida@hotmail.com>
2018-11-02 01:17:30 -0300

    conexão com o banco

--- server.js
+++ server.js
@@ -1,9 +1,25 @@
 const express = require('express');
+const mongose = require('mongoose');
+const requireDir = require('require-dir');
 
+//iniciando o app
 const app = express();
 
+//iniciando o db
+mongose.connect('mongodb://localhost:27017/nodeapi', { useNewUrlParser: true });
+requireDir('./src/models');
+
+const Product = mongose.model('Product');
+
+//primeira rota
 app.get('/',(req, res) => {
-    res.send('Hello Rocketsea    t!');
+
+    Product.create({title: 'React native',
+                     description: 'Build Native Apps with React',
+                        url: 'http://github.com/facebook/react-native'
+                    });
+
+    return res.send('Hello Rocketsea    t!');
 });
 
 app.listen(3001);
