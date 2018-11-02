a4009a97453a30ea601d61558a32364f5843b375
Cyntia <cyntia.aparecida@hotmail.com>
2018-11-02 14:03:36 -0300

    cors - final do curso

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const mongose = require('mongoose');
+const cors = require('cors');
 const requireDir = require('require-dir');
 
 //iniciando o app
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 //iniciando o db
 mongose.connect('mongodb://localhost:27017/nodeapi', { useNewUrlParser: true });
